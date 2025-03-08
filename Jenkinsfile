dc4612a0293b2880488c6b203ad33d8c62e2d02a
feulertovar <feuler@users-MacBook-Air.local>
2025-03-08 14:54:48 -0500

    updated hello.py && tests suites

--- Jenkinsfile
+++ Jenkinsfile
@@ -10,7 +10,8 @@ pipeline {
         stage('Test') {
             steps {
                 echo 'Running tests...'
-                sh 'python3 test.py'
+                sh 'python3 -m unittest test.py
+'
             }
         }
     }
--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 def say_hello(name="Jenkins"):
     """
     Returns a greeting message. If no name is provided, defaults to "Jenkins".
+    If an empty string is provided, defaults to "Jenkins".
+    Raises TypeError if the input is not a string.
     """
+    if not isinstance(name, str):  # Check if the input is not a string
+        raise TypeError("Input must be a string.")
+    if not name:  # If the name is an empty string or None, default to 'Jenkins'
+        name = "Jenkins"
     return f"Hello, {name}!"
